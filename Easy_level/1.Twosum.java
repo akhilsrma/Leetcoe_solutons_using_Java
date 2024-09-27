376b911889f90833f1ee0c1812e9c110d40c9829
Akhil Sharma <165511821+akhilsrma@users.noreply.github.com>
2024-09-28 00:34:30 +0530

    Add files via upload

---
+++ Easy_level/1.Twosum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] arr=new int [2];
+        for(int i=1;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]+nums[j-i]==target){
+                    arr[0]=j;
+                    arr[1]=j-i;
+                    return arr;
+                }
+            }
+        }
+        return null;
+    }
+}
